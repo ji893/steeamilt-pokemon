c04c02a61f2786a2d7199cf9f84a25f5b1b4762a
ji893 <143772343+ji893@users.noreply.github.com>
2025-04-30 09:20:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,33 +42,12 @@ pokemon = {
     "image_url": "https://i.namu.wiki/i/0KC24R7hvHoRQFaki5E9aJJc4h4NGh0szPAL9G7XDNPc6RiIdf7qCGfJkjrv3usF-ci2LLqQgxiFr1n7WTcbfYFKpWDnSyeVI8uUDBWwZ7-0V8hkd0VTPcms-NKxQXR3FEjJfQD8aJ40UW48XI8Qig.webp"
 }
 
-# 폼에 고유한 key 값을 부여하기 위해 key 값을 동적으로 변경합니다.
-with st.form(key="form1"):  # 고유한 key 사용
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        name = st.text_input(
-            label="포켓몬 이름",
-            value=example_pokemon["name"] if auto_complete else ""
-        )
-    
-    with col2:
-        types = st.multiselect(
-            label="포켓몬 속성",
-            options=list(type_emoji_dict.keys()),
-            max_selections=2,
-            default=example_pokemon["types"] if auto_complete else []
-        )
-    
-    image_url = st.text_input(
-        label="포켓몬 이미지 URL",
-        value=example_pokemon["image_url"] if auto_complete else ""
-    )
-
 with st.expander(label=pokemon["name"], expanded=True):
     st.image(pokemon["image_url"])
     emoji_types = [f"{type_emoji_dict[x]} {x}" for x in pokemon["types"]]
     st.subheader(f" / ".join(emoji_types))
+
+
 
 pokemons = [
     {
